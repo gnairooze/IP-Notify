a1b0d5995a6ca138b5d073b166d2c17435eb7660
George Nairooze <gnairooze@gmail.com>
2021-03-29 16:46:31 +0200

    change the public IP provider "https://ip.seeip.org"

--- IP-Notify/Properties/Settings.Designer.cs
+++ IP-Notify/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace IP_Notify.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -37,7 +37,7 @@ namespace IP_Notify.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("https://ifconfig.me/ip")]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://ip.seeip.org")]
         public string PublicIP_URL {
             get {
                 return ((string)(this["PublicIP_URL"]));
